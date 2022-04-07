f186de845a3f6cef5815466cbdddf23cf27303b1
jcattanach <jackcattanach@live.com>
2022-04-07 00:19:34 -0500

    initial check in

---
+++ src/main/java/com/jackcattanach/PokerTool/PokerToolApplication.java
@@ -0,0 +1,13 @@
+package com.jackcattanach.PokerTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackcattanach/PokerTool/PokerToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackcattanach.PokerTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
